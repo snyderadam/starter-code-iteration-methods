dbc063bf62db69b698b69cadb2c1fa421ab20176
Adam Snyder <snyderadam@gmail.com>
2020-07-03 17:27:51 -0400

    final commit

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+function printKata (kataNumber, object) {
+    // For the usage of the DETAILS and SUMMARY tags
+    // in HTML, see: http://mdn.io/details-element
+    const detailsElement = document.createElement('details')
+    main.append(detailsElement)
+    //
+    const summaryElement = document.createElement('summary')
+    summaryElement.append("KATA " + kataNumber)
+    detailsElement.append(summaryElement)
+    //
+    // http://mdn.io/json.stringify
+    const stringifiedObject = JSON.stringify(object)
+    detailsElement.append(stringifiedObject)
+}
+
+const userInactive = users.filter(user => user.isActive)
+printKata(1, userInactive)
+
+const userEmail = users.map(user => user.email)
+printKata(2, userEmail)
+
+const ovation = users.some(user => user.company === "ovation")
+printKata(3, ovation)
+
+const userAge = users.find(user => user.age > "38")
+printKata(4, userAge)
+
+const userActive = users.filter(user => user.isActive)
+                        .find(user => user.age > "38")
+printKata(5, userActive)
+
+const userCompany = users.filter(user => user.company === "ZENCO")
+                         .map(user => user.balance)
+printKata(6, userCompany)
+
+const fugiat = users.filter(user => user.tags.includes('fugiat'))
+                    .map(user => user.age)
+printKata(7, fugiat)
